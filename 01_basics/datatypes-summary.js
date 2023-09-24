81bd35b2d2cbbc3f8311a22d83cd2c61a5fdb39f
priyeranj <71518611+priyeranj@users.noreply.github.com>
2023-09-24 05:49:17 +0000

    datatypes -summary added

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -3,8 +3,39 @@
 // 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
 
 const score = 100;
+console.log(typeof score);
+
+const scoreValue = 100.3;
+console.log(typeof scoreValue);
+
+const isLoggedIn = false;
+console.log(typeof isLoggedIn);
+
+const outsideTemp = null;
+console.log(typeof outsideTemp);
+
+let userEmail;
+console.log(typeof userEmail);
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(anotherId);
 
 
 // Reference (Non primitive)
 
 // Array,Objects, Functions
+
+const heroes = ["Shaktiman","naagraj","doga"]
+
+let myObj = {
+    name :"hitesh",
+    age: 22,
+}
+console.log(typeof myObj);
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof myFunction);
