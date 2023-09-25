10ed98f1bf7404bf92305730ef515905f207388c
priyeranj <71518611+priyeranj@users.noreply.github.com>
2023-09-25 15:46:20 +0000

    finalize arrays topics

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,31 @@
+const marvel_heroes = ["thor", "Ironman", "Spiderman"]
+const dc_heroes =["superman","flash","batman"]
+
+// marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes);
+
+const newDcHeroes = marvel_heroes.concat(dc_heroes);
+// console.log(newDcHeroes);
+
+const all_new_heroes = [...marvel_heroes, ...dc_heroes];
+// console.log(all_new_heroes);
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+
+const real_another_array  = another_array.flat(Infinity);
+// console.log(real_another_array);
+
+console.log(Array.isArray("Ranjan"));
+console.log(Array.from("Hitesh"));
+console.log(Array.from({name: "Hitesh"})); //intresting
+
+let score1 = 100;
+let score2 = 900;
+let score3 = 1800;
+
+console.log(Array.of(score1,score2,score3));
+
+
+
+
+
