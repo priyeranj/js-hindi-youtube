1aeb36b82470b667edd95677f9d3e0b9b06403bf
priyeranj <71518611+priyeranj@users.noreply.github.com>
2023-09-25 04:23:49 +0000

    final commit

--- 01_basics/07_Data_Time.js
+++ 01_basics/07_Data_Time.js
@@ -7,5 +7,13 @@ let myDate = new Date();
 
 // console.log(typeof myDate);
 
-let myCreatedDate = new Date(2023, 0, 23);
-console.log(myCreatedDate.toLocaleString());
+// let myCreatedDate = new Date(2023, 0, 23);
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3);
+let myCreatedDate = new Date("2023-01-14");
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
