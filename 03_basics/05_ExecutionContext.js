e73b855bf09a6be7cf1ca997a582adda6260613b
priyeranj <71518611+priyeranj@users.noreply.github.com>
2023-09-27 13:35:02 +0000

    final basic

--- 03_basics/05_ExecutionContext.js
+++ 03_basics/05_ExecutionContext.js
@@ -8,3 +8,20 @@
         //-->Execution phase
 
         
+        function one(){
+            console.log("one")
+            two()
+        }
+        function two() {
+            console.log("two")
+            three()
+        }
+        function three(){
+            console.log("three")
+        }
+        
+        one()
+        two()
+        three()
+
+        
