844fa79345a077b05a0f8ddb8171151f23cc8b29
priyeranj <71518611+priyeranj@users.noreply.github.com>
2023-09-27 13:32:47 +0000

    completed EC

---
+++ 03_basics/05_ExecutionContext.js
@@ -0,0 +1,10 @@
+            //Javascript Execution Context
+        //-->Global EC->this stores the execution context of 
+        //-->Function EC
+        //-->Eval EC
+
+        //Phases of js code execution
+        //-->Memory allocation phase
+        //-->Execution phase
+
+        
