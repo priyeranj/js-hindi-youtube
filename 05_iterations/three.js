2061f59b3f567479b2b8df3f02bf4eb89c2a6094
priyeranj <71518611+priyeranj@users.noreply.github.com>
2023-09-28 10:49:29 +0000

    commited loops

---
+++ 05_iterations/three.js
@@ -0,0 +1,42 @@
+// for of
+
+// ["","",""]
+// [{},{},{}]
+
+const arr = [1,2,3,4,5]
+
+// for (const val of arr) {
+//     console.log(val);
+// }
+
+const greetings="Hello worrld!";
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);
+}
+
+//Maps
+
+const map = new Map();
+map.set('IN',"INDIA")
+map.set('USA',"United States of America")
+map.set('Fr',"France")
+map.set('IN',"India")
+
+// console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key,":-",value);
+}
+
+
+const myObject = {
+    'game1':'NFS',
+    'game2':'Spiderman'
+}
+
+
+// for (const [key,value] of myObject) {
+//     console.log(key, ":-",value);
+// }
+
+
